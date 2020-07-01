332ac3bd5bc55e06cf0c3fee48fb44d37ab2e0fc
Dariusz Wojtowicz <dariusz.wojtowicz@allegro.pl>
2020-07-01 09:35:49 +0200

    Redux, working tests

--- src/components/tests/UserInfoRedux.spec.jsx
+++ src/components/tests/UserInfoRedux.spec.jsx
@@ -3,11 +3,30 @@ import React from 'react';
 import {UserInfoRedux} from "../UserInfoRedux";
 import {Typography} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
+import {Provider} from "react-redux";
+import configureStore from 'redux-mock-store';
+
+const user = {
+  name: 'Darek',
+  lastName: 'Wójtowicz',
+  age: 28,
+  login: 'dariuszwojtowicz',
+  email: 'dariusz.wojtowicz@hotmail.com'
+};
+const mockStore = configureStore([]);
+const dispatchMock = () => Promise.resolve({});
+const store = mockStore({
+  currentUser: user
+});
+store.dispatch = jest.fn(dispatchMock);
 
 describe('UserInfoBasic', () => {
   describe('Initial state', () => {
     const wrapper = mount(
-      <UserInfoRedux />
+      <Provider store={store}>
+        <UserInfoRedux />
+      </Provider>,
+      { context: { store } }
     );
 
     test('should render header with full name', () => {
@@ -25,7 +44,10 @@ describe('UserInfoBasic', () => {
 
   describe(`After 'Show user details' button click`, () => {
     const wrapper = mount(
-      <UserInfoRedux user={user} />
+      <Provider store={store}>
+        <UserInfoRedux />
+      </Provider>,
+      { context: { store } }
     );
     wrapper.find(Button).simulate('click');
 
@@ -35,6 +57,17 @@ describe('UserInfoBasic', () => {
 
     test(`should render 'Hide user details' button`, () => {
       expect(wrapper.find(Button).text()).toEqual('Hide user details');
+    });
+
+    test(`should render 'Hide user details' button`, () => {
+      // when
+      wrapper.find('input#email').simulate('change', { target: { value: 'new@email.com' }});
+
+      // then
+      expect(store.dispatch).toHaveBeenCalledWith( {
+        email: "new@email.com",
+        type: "UPDATE_EMAIL"
+      });
     });
   });
 });
